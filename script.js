d60079c8cba20805971ba0df71c35ba15fe8e3d6
anushaanand19 <anushaanand1991@gmail.com>
2020-12-05 17:02:19 +0530

    Implemented Joke API

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
+const speech = window.speechSynthesis;
 const button = document.getElementById('button');
-button.addEventListener('click', async function() {console.log("works")});
+const apiUrl = "https://joke3.p.rapidapi.com/v1/joke"
+
+function textToSpeech(joke) {
+    let utterance = new SpeechSynthesisUtterance(joke.content);
+    console.log(utterance);
+    speech.speak(utterance); 
+    utterance.onstart = () => {
+        button.disabled = true;
+        console.log('speech started', button.disabled, utterance.onstart);
+    }
+    utterance.onend = () => {
+        button.disabled = false;
+        console.log('speech ended', button.disabled)
+    }
+}
+
+async function getJokes() {
+    const headers = await fetch("headers.json");
+    let data = await headers.json();
+    const getJoke = await fetch(apiUrl, {
+        "method": "GET",
+        "headers": data
+    });
+    const joke = await getJoke.json();
+    textToSpeech(joke);   
+}
+
+button.addEventListener('click', getJokes);
+
