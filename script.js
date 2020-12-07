7d91f6dcd62f10e49c9b00208f04e74f468e43bd
anushaanand19 <anushaanand1991@gmail.com>
2020-12-07 12:22:45 +0530

    Text display of joke

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const speech = window.speechSynthesis;
 const button = document.getElementById('button');
 const apiUrl = "https://joke3.p.rapidapi.com/v1/joke"
+const jokeContent = document.getElementById('joke-content');
+const jokeContainer = document.getElementById('joke-container');
 
 //Disable/Enable button
 function toggleButton() {
     button.disabled = !button.disabled;
 }
 
+function displayJoke(joke) {
+    jokeContainer.hidden = false;
+    jokeContent.hidden = false;
+    jokeContent.innerText = joke.content;
+    console.log(button);
+}
+
 function textToSpeech(joke) {
     const utterance = new SpeechSynthesisUtterance(joke.content);
     speech.speak(utterance); 
-    utterance.onstart = () => toggleButton();
-    utterance.onend = () => toggleButton();
-}
+    utterance.onstart() 
+    {
+        toggleButton();
+    } 
+    utterance.onend()
+    {
+        toggleButton();
+    }
+};
 
 async function getJokes() {
     const headers = await fetch("headers.json");
@@ -22,6 +37,11 @@ async function getJokes() {
         "headers": data
     });
     const joke = await getJoke.json();
+    if (navigator.userAgent.indexOf("Chrome") != -1) {
+        button.setAttribute('href', '#joke-container');
+        displayJoke(joke);
+        return;
+    }
     textToSpeech(joke);   
     
 }
